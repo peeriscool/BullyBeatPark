f7575e18fb5fa1a57877a4e622a3b3212ce0310b
peeriscool <44834355+peeriscool@users.noreply.github.com>
2022-03-08 00:43:46 +0100

    Fixed animator issue
    animations now play whith player input

--- Assets/Scripts/ControlToAnimator.cs
+++ Assets/Scripts/ControlToAnimator.cs
@@ -5,10 +5,12 @@ using UnityEngine.InputSystem;
 public class ControlToAnimator : MonoBehaviour
 {
     public Animator linkedRig;
+    private string currentState;
     AnimatorControllerParameter[] a;
     // Start is called before the first frame update
     void Start()
     {
+        //linkedRig.runtimeAnimatorController.animationClips()
         //foreach (var item in linkedRig.parameters)
         //{
         //    Debug.Log(item.name);
@@ -16,12 +18,31 @@ public class ControlToAnimator : MonoBehaviour
 
         a = linkedRig.parameters;
     }
-
+    private void Update()
+    {
+        keycheck();
+    }
+    private void FixedUpdate()
+    {
+     if(linkedRig.GetCurrentAnimatorStateInfo(0).IsName("mesh_001|walk"))
+        {
+            Debug.Log(linkedRig.GetCurrentAnimatorStateInfo(0).IsName("mesh_001|walk"));
+            //linkedRig. .Play("mesh_001|walk");
+            linkedRig.GetCurrentAnimatorStateInfo(0).Equals("mesh_001|walk");
+        }
+    }
     // Update is called once per frame
-    void Update()
+    void ChangeAnimationState(string newState)
+    {
+        if (currentState == newState) return;
+        linkedRig.Play(newState);
+        currentState = newState;
+    }
+    void keycheck()
     {
         if (Keyboard.current.wKey.wasPressedThisFrame)
         {
+            ChangeAnimationState("mesh_001|walk");
             //linkedRig.Play("mesh_001|walk");
             linkedRig.SetTrigger("Walk") ;
         }
@@ -32,16 +53,20 @@ public class ControlToAnimator : MonoBehaviour
 
         if (Keyboard.current.shiftKey.wasPressedThisFrame)
         {
+            ChangeAnimationState("mesh_001|walk");
             //set walk speed to x2
             linkedRig.SetTrigger("Run");
+            linkedRig.speed = 2;
         }
         if (Keyboard.current.shiftKey.wasReleasedThisFrame)
         {
             linkedRig.ResetTrigger("Run");
+            linkedRig.speed = 1;
         }
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
-            linkedRig.Play("mesh_001|Dive and get up");
+            ChangeAnimationState("mesh_001|Dive and get up");
+           // linkedRig.Play("mesh_001|Dive and get up");
             linkedRig.SetTrigger("Dive");
         }
         if (Keyboard.current.spaceKey.wasReleasedThisFrame)
@@ -50,7 +75,8 @@ public class ControlToAnimator : MonoBehaviour
         }
         if (Keyboard.current.eKey.wasPressedThisFrame)
         {
-            linkedRig.Play("mesh_001|Zombie_Punch");
+            ChangeAnimationState("mesh_001|Zombie_Punch");
+           // linkedRig.Play("mesh_001|Zombie_Punch");
             linkedRig.SetTrigger("punch");
             //punch
         }
@@ -58,7 +84,5 @@ public class ControlToAnimator : MonoBehaviour
         {
             linkedRig.ResetTrigger("punch");
         }
-
-
     }
 }
